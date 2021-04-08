0ca36686fd5fc5759cec1aaaa2c697d8f21cd387
DESKTOP-EV55PUC\oscar <oscarht@live.nl>
2021-04-08 12:53:05 -0400

    minor

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ import pygetwindow as gw
 from fundamentals.initialization import open_vba, config, start_bot
 from fundamentals.controls import *
 
-start_bot(console_level='DEBUG')
+start_bot(console_level='INFO')
 
