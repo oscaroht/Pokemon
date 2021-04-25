ee8f02fef2ca753c267f99678b4f19e851a2bd43
DESKTOP-EV55PUC\oscar <oscarht@live.nl>
2021-04-25 10:25:33 +0200

    s + w table to db

---
+++ derivatives/strength_weakness_matrix_to_db.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+import os
+from fundamentals.config import config
+
+from sqlalchemy import create_engine
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+engine = create_engine(
+    f"postgresql+psycopg2://postgres:{config('../users.ini', 'postgres', 'password')}@localhost/pokemon")
+
+df = pd.read_csv('C:\\Users\\oscar\\PycharmProjects\\Pokemon\\db_mappings\\strength_weakness.csv')
+
+df = pd.melt(df,id_vars='atk',var_name='def',value_name='multiplier', ignore_index=True)
+df = df.apply(lambda x: x.astype(str).str.lower())
+
+df.to_sql('strength_weakness',schema='mart',con=engine,if_exists='append',index= False)
