93891ebbae7d3f3aee2288d1eb9e00e4cecb0430
DESKTOP-EV55PUC\oscar <oscarht@live.nl>
2022-06-10 00:06:11 +0200

    add short cuts

--- pokebot/short_cuts.py
+++ pokebot/short_cuts.py
@@ -3,6 +3,12 @@ oak = ('oaks_lab', 26, 'up')
 brock = ('pewter_city_gym', 15, 'up')
 viridian_city_pc = ('viridian_city_pc', 4, 'up')
 pewter_city_pc = ('pewter_city_pc', 4, 'up')
+route3b_pc = ('route3b_pc', 4, 'up')
+cerulean_city_pc = ('cerulean_city_pc', 4, 'up')
+
+helix_fossil = ('mount_moon3', 197, 11,5)
+dome_fossil = ('mount_moon3', 196, 10,5)
+
 viridian_city_market = ('viridian_city_market', 27, 'left')
 starter_pokemon_location = {'charmander': ('oaks_lab', 37, 'up'),
                              'squirtle': ('oaks_lab', 38, 'up'),
